b54ce3b4b38fb584798d8d62884cc5908829939a
Oliver Lau <oliver@von-und-fuer-lau.de>
2013-04-16 08:36:20 +0200

    INTERIM: GLClass überarbeitet.

--- glclass.cpp
+++ glclass.cpp
@@ -26,64 +26,64 @@ GLClass::~GLClass()
     /* ... */
 }
 
-void GLClass::uniform1f(const char* location, GLfloat v0)
+void GLClass::uniform1f(const QString& location, GLfloat v0)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0);
 }
 
-void GLClass::uniform2f(const char* location, GLfloat v0, GLfloat v1)
+void GLClass::uniform2f(const QString& location, GLfloat v0, GLfloat v1)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, QVector2D(v0, v1));
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), QVector2D(v0, v1));
 }
 
-void GLClass::uniform3f(const char* location, GLfloat v0, GLfloat v1, GLfloat v2)
+void GLClass::uniform3f(const QString& location, GLfloat v0, GLfloat v1, GLfloat v2)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, QVector3D(v0, v1, v2));
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), QVector3D(v0, v1, v2));
 }
 
-void GLClass::uniform4f(const char* location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3)
+void GLClass::uniform4f(const QString& location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, QVector4D(v0, v1, v2, v3));
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), QVector4D(v0, v1, v2, v3));
 }
 
-void GLClass::uniform1i(const char* location, GLint v0)
+void GLClass::uniform1i(const QString& location, GLint v0)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0);
 }
 
-void GLClass::uniform2i(const char* location, GLint v0, GLint v1)
+void GLClass::uniform2i(const QString& location, GLint v0, GLint v1)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0, v1);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0, v1);
 }
 
-void GLClass::uniform3i(const char* location, GLint v0, GLint v1, GLint v2)
+void GLClass::uniform3i(const QString& location, GLint v0, GLint v1, GLint v2)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0, v1, v2);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0, v1, v2);
 }
 
-void GLClass::uniform4i(const char* location, GLint v0, GLint v1, GLint v2, GLint v3)
+void GLClass::uniform4i(const QString& location, GLint v0, GLint v1, GLint v2, GLint v3)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0, v1, v2, v3);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0, v1, v2, v3);
 }
 
-void GLClass::uniform1ui(const char* location, GLuint v0)
+void GLClass::uniform1ui(const QString& location, GLuint v0)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0);
 }
 
-void GLClass::uniform2ui(const char* location, GLuint v0, GLuint v1)
+void GLClass::uniform2ui(const QString& location, GLuint v0, GLuint v1)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0, v1);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0, v1);
 }
 
-void GLClass::uniform3ui(const char* location, GLuint v0, GLuint v1, GLuint v2)
+void GLClass::uniform3ui(const QString& location, GLuint v0, GLuint v1, GLuint v2)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0, v1, v2);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0, v1, v2);
 }
 
-void GLClass::uniform4ui(const char* location, GLuint v0, GLuint v1, GLuint v2, GLuint v3)
+void GLClass::uniform4ui(const QString& location, GLuint v0, GLuint v1, GLuint v2, GLuint v3)
 {
-    mRenderWidget->shaderProgram()->setUniformValue(location, v0, v1, v2, v3);
+    mRenderWidget->shaderProgram()->setUniformValue(location.toLatin1().constData(), v0, v1, v2, v3);
 }
 
 void GLClass::setVertexShader(const QString& vs)
@@ -91,7 +91,7 @@ void GLClass::setVertexShader(const QString& vs)
     mRenderWidget->setVertexShader(vs);
 }
 
-const QString &GLClass::vertexShader() const
+const QString& GLClass::vertexShader(void) const
 {
     return mRenderWidget->vertexShader();
 }
@@ -101,7 +101,7 @@ void GLClass::setFragmentShader(const QString& fs)
     mRenderWidget->setFragmentShader(fs);
 }
 
-const QString &GLClass::fragmentShader() const
+const QString& GLClass::fragmentShader(void) const
 {
     return mRenderWidget->fragmentShader();
 }
--- glclass.h
+++ glclass.h
@@ -29,18 +29,18 @@ public:
     }
 
 public slots:
-    void uniform1f(const char* location, GLfloat v0);
-    void uniform2f(const char* location, GLfloat v0, GLfloat v1);
-    void uniform3f(const char* location, GLfloat v0, GLfloat v1, GLfloat v2);
-    void uniform4f(const char* location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
-    void uniform1i(const char* location, GLint v0);
-    void uniform2i(const char* location, GLint v0, GLint v1);
-    void uniform3i(const char* location, GLint v0, GLint v1, GLint v2);
-    void uniform4i(const char* location, GLint v0, GLint v1, GLint v2, GLint v3);
-    void uniform1ui(const char* location, GLuint v0);
-    void uniform2ui(const char* location, GLuint v0, GLuint v1);
-    void uniform3ui(const char* location, GLuint v0, GLuint v1, GLuint v2);
-    void uniform4ui(const char* location, GLuint v0, GLuint v1, GLuint v2, GLuint v3);
+    void uniform1f(const QString& location, GLfloat v0);
+    void uniform2f(const QString& location, GLfloat v0, GLfloat v1);
+    void uniform3f(const QString& location, GLfloat v0, GLfloat v1, GLfloat v2);
+    void uniform4f(const QString& location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+    void uniform1i(const QString& location, GLint v0);
+    void uniform2i(const QString& location, GLint v0, GLint v1);
+    void uniform3i(const QString& location, GLint v0, GLint v1, GLint v2);
+    void uniform4i(const QString& location, GLint v0, GLint v1, GLint v2, GLint v3);
+    void uniform1ui(const QString& location, GLuint v0);
+    void uniform2ui(const QString& location, GLuint v0, GLuint v1);
+    void uniform3ui(const QString& location, GLuint v0, GLuint v1, GLuint v2);
+    void uniform4ui(const QString& location, GLuint v0, GLuint v1, GLuint v2, GLuint v3);
 
     void setVertexShader(const QString&);
     const QString& vertexShader(void) const;
